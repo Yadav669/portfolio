85ebc7a73397e152a516f3d47a2c337be7c527df
Yadav669 <tion3256@gmail.com>
2024-01-04 10:22:19 +0530

    project page

--- src/components/Phero.js
+++ src/components/Phero.js
@@ -1,14 +1,15 @@
-import "./Phero.css"
- import React from 'react'
- 
- const Phero = () => {
-   return (
-     <div className="hero-img">
-       <h1>Projects</h1>
-       <p>Some od my most recent Work</p>
-     </div>
-   )
- }
- 
- export default Phero
- 
+import "./Phero.css";
+import React from "react";
+
+const Phero = () => {
+  return (
+    <div className="hero-img">
+      <div className="heading">
+        <h1>Projects</h1>
+        <p>Some od my most recent Work</p>
+      </div>
+    </div>
+  );
+};
+
+export default Phero;
